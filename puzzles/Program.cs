360783f6b297197422c05f227edc4bb038ed7559
Kevin McWhirter <klmcwhirter@gmail.com>
2017-08-03 22:46:48 -0700

    Eliminate warnings due to refactoring of startup config in 2.0; closes #1

--- puzzles/Program.cs
+++ puzzles/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using System.Linq;
+using Microsoft.Extensions.Logging;
 
 namespace puzzles
 {
@@ -11,17 +12,18 @@ namespace puzzles
     {
         public static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder()
-                .AddCommandLine(args)
-                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
-                .Build();
-
-            var hostBuilder = new WebHostBuilder()
-                .UseConfiguration(config)
+            var host = new WebHostBuilder()
+                .UseSetting(WebHostDefaults.PreventHostingStartupKey, "true")
+                .ConfigureLogging((context, factory) =>
+                {
+                    factory.AddConfiguration(context.Configuration.GetSection("Logging"));
+                    factory.AddConsole();
+                    factory.AddDebug();
+                })
                 .UseKestrel()
-                .UseUrls("http://*:5000/");
-
-            var host = hostBuilder
+                .UseUrls("http://*:5000/")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
 
