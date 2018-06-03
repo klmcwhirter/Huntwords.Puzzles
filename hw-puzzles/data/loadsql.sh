57924636df4200d2aa77d4f35cfabf0295300cd6
Kevin McWhirter <klmcwhirter@gmail.com>
2018-06-03 16:23:12 -0700

    Remove db related helper files

--- hw-puzzles/data/loadsql.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Loading $1 ..."
-cat $1 | sqlite3 Data/puzzles.sqlite
-echo -e "Loading $1 ... DONE."
--- hw-puzzles/data/redis/words2redis.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-gzip -dc words-all.gz | 
-awk '
-length($1) >= 3 && length($1) <= 15 && $1 ~ "^[A-Za-z0-9]+$" { printf("LPUSH \"urn:words\" \"%s\"\r\n", $1); }
-' | sort -ru --ignore-case | (echo -en "LTRIM \"urn:words\" -1 0\r\n"; cat -)
