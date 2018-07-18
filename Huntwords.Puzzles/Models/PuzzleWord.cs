03f8afed837309b237b8ddc012f1d8eceeb94080
Kevin McWhirter <klmcwhirter@gmail.com>
2018-07-17 19:48:19 -0700

    Bumped Huntwords.Common up to 2.0.12 to take advantage of new pubsub semantics; add XML comments to PuzzleWord

--- Huntwords.Puzzles/Models/PuzzleWord.cs
+++ Huntwords.Puzzles/Models/PuzzleWord.cs
@@ -1,7 +1,15 @@
 namespace Huntwords.Puzzles.Models
 {
+    /// <summary>
+    /// Represents a word in a puzzle
+    /// </summary>
+    /// <remarks>Used in the add word API</remarks>
     public class PuzzleWord
     {
+        /// <summary>
+        /// the word to add
+        /// </summary>
+        /// <value>string</value>
         public string Word { get; set; }
     }
 }
